d344e397b458c0af45c390e90a8b58c908175174
Patroick <43811779+Patroick@users.noreply.github.com>
2021-11-15 21:39:06 +0100

    Commit 15.11 21:38

---
+++ Spring-boot-tutorial/src/main/java/com/patrick/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -0,0 +1,30 @@
+package com.patrick.Springboot.tutorial.service;
+
+import com.patrick.Springboot.tutorial.entity.Department;
+import com.patrick.Springboot.tutorial.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
+    }
+
+    @Override
+    public Department fetchDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId).get();
+    }
+}
